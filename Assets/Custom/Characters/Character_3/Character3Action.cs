26d01cc6366f7b6be411f916e2b00578e3fc66c1
Jinje Han <42795430+coffeetea99@users.noreply.github.com>
2025-05-25 20:28:35 +0900

    Character (#5)
    * add character + textures & change into humanoid
    
    * import animations
    
    * make animations humanoid & loop
    
    * add character to scene & create animator controller & add talking movement
    
    * edit animator & create animation script
    
    * add Canvas & Restarter script
    
    * reset character on restart
    
    * add other character assets
    
    * add script, animation controller, etc to other characters

---
+++ Assets/Custom/Characters/Character_3/Character3Action.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Character3Action : MonoBehaviour
+{
+    private const int IDLE = 0;
+    private const int TALKING = 1;
+    private const int WALKING = 2;
+    private const int RUNNING = 3;
+    private const int SEARCHING = 4;
+    private const int CLICKING = 5;
+
+    public Animator animator;
+
+    private float timer = 0f;
+    private float beforeTime;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        Transform transform = GetComponent<Transform>();
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Transform transform = GetComponent<Transform>();
+            transform.position = initialPosition;
+            transform.rotation = initialRotation;
+        }
+
+        beforeTime = timer;
+        timer += Time.deltaTime;
+
+        // TODO: fix
+        if (DidTimePass(3))
+        {
+            SetAction(TALKING);
+        }
+    }
+
+    bool DidTimePass(float referenceTime)
+    {
+        return beforeTime < referenceTime && referenceTime <= timer;
+    }
+
+    void SetAction(int action)
+    {
+        animator.SetInteger("Action", action);
+    }
+}
