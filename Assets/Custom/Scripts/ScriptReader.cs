d4dd489cda70340b8aa8a93f15a4d76eb7979c65
coffeetea99 <coffeetea99iro@gmail.com>
2025-05-25 17:24:52 +0900

    add ScriptLine struct in code

--- Assets/Custom/Scripts/ScriptReader.cs
+++ Assets/Custom/Scripts/ScriptReader.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
 using TMPro;
 using System;
+using System.Linq;
+
+struct ScriptLine
+{
+    public int timeSecond;
+    public string text;
+
+    public ScriptLine(int _timeSecond, string _text)
+    {
+        timeSecond = _timeSecond;
+        text = _text;
+    }
+}
 
 public class ScriptReader : MonoBehaviour
 {
@@ -18,6 +31,13 @@ public class ScriptReader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mText.text = String.Join("\n", new string[] { "A : 찾았어?", "B : 아니, 아직.", "A : 큰일 났네.", "B : 어떡하지?\n" });
+        mText.text = String.Join("\n", fullScript.Select(line => line.text));
     }
+
+    private static readonly ScriptLine[] fullScript = new ScriptLine[]{
+        new ScriptLine(1, "A : 찾았어?"),
+        new ScriptLine(2, "B : 아니, 아직."),
+        new ScriptLine(3, "A : 큰일 났네."),
+        new ScriptLine(4, "B : 어떡하지?\n")
+    };
 }
