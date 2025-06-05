bcb14f74b7e27572fbd3ade46e1ee51075879e3e
Jinje Han <42795430+coffeetea99@users.noreply.github.com>
2025-06-05 15:59:15 +0900

    Input (#11)
    * remove moving object and its script
    
    * add floor collider
    
    * add script for left controller X button
    
    * add script for left controller Y button
    
    * disable former canvas and create new one
    
    * disable Mesh Renderer of TunnelingVignette
    
    * change y transform of XR Origin and Camera Offset

--- Assets/Custom/Scripts/ScriptUI.cs
+++ Assets/Custom/Scripts/ScriptUI.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class ScriptUI : MonoBehaviour
 {
@@ -8,21 +10,50 @@ public class ScriptUI : MonoBehaviour
 
     private bool isVisible = true;
 
+    private InputDevice leftController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         setVisibility();
+
+        // Try to get the left controller
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, devices);
+
+        if (devices.Count > 0)
+        {
+            leftController = devices[0];
+            Debug.Log("Left controller found: " + leftController.name);
+        }
+        else
+        {
+            Debug.LogWarning("Left controller not found.");
+        }
     }
 
     // Update is called once per frame
     // TODO: change key
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+#if UNITY_EDITOR
+        // Editor test: simulate X button press with the 'X' key
+        if (Input.GetKeyDown(KeyCode.X))
         {
             isVisible = !isVisible;
             setVisibility();
         }
+#else
+        if (!leftController.isValid)
+            return;
+
+        // Check if the X button (primaryButton on left controller) is pressed
+        if (leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool xButtonPressed) && xButtonPressed)
+        {
+            isVisible = !isVisible;
+            setVisibility();
+        }
+#endif
     }
 
     void setVisibility()
